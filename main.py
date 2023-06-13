4351f222950c34cbb226854987de64f2e882bbdb
sndpwrites <deep.newsun@gmail.com>
2023-06-13 14:55:43 +0545

    added pomodoro_timer.py

--- main.py
+++ main.py
@@ -83,38 +83,6 @@ def func_calculate_pi_digits(digits):
         return 22/7
 
 
-def pomodoro_timer(numberOfReps):
-    print("\t##POMODORO TIMER##")
-    print("25 minutes of work followed by")
-    print("5 minutes of break")
-    work_remaining = 25 * 60
-    rest_remaining = 5 * 60
-
-    print("Number of Pomodoros remaining: " + str(numberOfReps))
-    while numberOfReps > 0:
-        print("Time remaining until next REST")
-        while work_remaining > 0:
-            timer = datetime.timedelta(seconds=work_remaining)
-            print(timer, end="\r")
-            time.sleep(1)
-            work_remaining -= 1
-        print("Work time finished. Please rest now!")
-        print("Time remaining until next WORK")
-        while rest_remaining > 0:
-            timer = datetime.timedelta(seconds=rest_remaining)
-            print(timer, end="\r")
-            time.sleep(1)
-            rest_remaining -= 1
-        print("Rest time finished. Back to work!")
-        numberOfReps -= 1
-        pomodoro_timer(numberOfReps)
-
-    print("All pomodoros completed. Please take a longer rest and come back\n")
-    time.sleep(5)
-
-
-# pomodoro_timer(4)
-
 def apply_rot13(message):
     mapping = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
                             'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
