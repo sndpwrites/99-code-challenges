0f26632b09abbf7054dc3567f510d819e0f5b185
sndpwrites <deep.newsun@gmail.com>
2024-08-06 17:18:15 -0400

    refactored color_scheme_generator

--- color_scheme_generator.py
+++ color_scheme_generator.py
@@ -1,17 +1,13 @@
 import random
 
-
-def get_random_value():
-    return random.randint(0, 255)
-
+def get_random():
+    return random.randint(0, 256)
 
 def rgb_to_hex(red, green, blue):
     return '#{:02x}{:02x}{:02x}'.format(red, green, blue)
 
+def generate_color_scheme(color_hex):
+    return f"https://www.color-hex.com/color/{color_hex.removeprefix('#')}"
 
-red = get_random_value()
-green = get_random_value()
-blue = get_random_value()
-
-color_hex = rgb_to_hex(red, green, blue)
-print("https://www.color-hex.com/color/"+str(color_hex).removeprefix('#'))
+new_color = generate_color_scheme(rgb_to_hex(get_random(), get_random(), get_random()))
+print(new_color)
